479fb4945059b71c5490233a5d1e5e788e30c413
Ryan Bauman <rbauman@lgsinnovations.com>
2017-06-12 14:18:50 -0400

    RELENG-623 - add gitlab ci support; bump version post release

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #
-# This file is protected by Copyright. Please refer to the COPYRIGHT file distributed with this 
+# This file is protected by Copyright. Please refer to the COPYRIGHT file distributed with this
 # source distribution.
-# 
+#
 # This file is part of REDHAWK Basic Components agc.
-# 
-# REDHAWK Basic Components agc is free software: you can redistribute it and/or modify it under the terms of 
-# the GNU Lesser General Public License as published by the Free Software Foundation, either 
+#
+# REDHAWK Basic Components agc is free software: you can redistribute it and/or modify it under the terms of
+# the GNU Lesser General Public License as published by the Free Software Foundation, either
 # version 3 of the License, or (at your option) any later version.
-# 
-# REDHAWK Basic Components agc is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+#
+# REDHAWK Basic Components agc is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 # PURPOSE.  See the GNU Lesser General Public License for more details.
-# 
-# You should have received a copy of the GNU Lesser General Public License along with this 
+#
+# You should have received a copy of the GNU Lesser General Public License along with this
 # program.  If not, see http://www.gnu.org/licenses/.
 #
 
@@ -22,9 +22,9 @@ if [ "$1" = "rpm" ]; then
     if [ -e rh.agc.spec ]; then
         mydir=`dirname $0`
         tmpdir=`mktemp -d`
-        cp -r ${mydir} ${tmpdir}/rh.agc-2.0.0
-        tar czf ${tmpdir}/rh.agc-2.0.0.tar.gz --exclude=".svn" -C ${tmpdir} rh.agc-2.0.0
-        rpmbuild -ta ${tmpdir}/rh.agc-2.0.0.tar.gz
+        cp -r ${mydir} ${tmpdir}/rh.agc-2.0.1
+        tar czf ${tmpdir}/rh.agc-2.0.1.tar.gz --exclude=".svn" -C ${tmpdir} rh.agc-2.0.1
+        rpmbuild -ta ${tmpdir}/rh.agc-2.0.1.tar.gz
         rm -rf $tmpdir
     else
         echo "Missing RPM spec file in" `pwd`
